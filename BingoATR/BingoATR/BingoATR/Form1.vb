15925fea88a4a4f5e8f5443b6a4cdce6111d0b05
fedehartwig <49241034+fedehartwig@users.noreply.github.com>
2019-06-08 13:32:04 -0300

    tablero 1.0

---
+++ BingoATR/BingoATR/BingoATR/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class frmBingo
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim botones As New List(Of Button)
+        Dim boton As New Button()
+        boton.Height = 55
+        boton.Width = 55
+        Dim a, b, c As Integer
+        For i = 0 To 9 Step 1
+            For j = 0 To 8 Step 1
+                a = j & i
+                boton.Text = a + 1
+                c = j + 1
+                b = i
+                boton.Location = New Point(b * 55, c * 55)
+                botones.Add(boton)
+                boton.Enabled = True
+                boton = New Button()
+                boton.Height = 55
+                boton.Width = 55
+                Me.Controls.AddRange(botones.ToArray())
+            Next
+        Next
+    End Sub
+End Class
+
